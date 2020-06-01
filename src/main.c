c68c9b2fb4277921a686bd6e92a6c959e242d3f9
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2020-05-31 21:32:26 -0500

    implement computations from datasheet, switched to using all floating point throughout, because the datasheet apparently didn't check their own computations and we're off by a little. not sure which will be the best bet

--- src/main.c
+++ src/main.c
@@ -5,7 +5,56 @@
 #include <unistd.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <math.h>
 #include "bmp_180.h"
+
+
+const int16_t  ac1 = 408;
+const int16_t  ac2 = -72;
+const int16_t  ac3 = -14383;
+const uint16_t ac4 = 32741;
+const uint16_t ac5 = 32757;
+const uint16_t ac6 = 23153;
+const int16_t  b1  = 6190;
+const int16_t  b2  = 4;
+const int16_t  mb  = -32768;
+const int16_t  mc  = -8711;
+const int16_t  md  = 2868;
+
+void compute_true_temp_and_pressure(
+			float* true_temp
+		, float* true_pressure
+		, const int32_t raw_temp
+		, const int32_t raw_pressure
+		, const int16_t oss
+		, const uint8_t cal[22]
+		)
+{
+	const float x11 = (raw_temp - ac6) * ac5 / (1 << 15);
+  const float x21 = mc * (1 << 11) / (x11 + md);
+  const float b5  = x11 + x21;
+	const float b6  = b5 - 4000;
+	const float x12 = (b2 * (b6 * b6 / (1 << 12))) / (1 << 11);
+	const float x22 = ac2 * b6 / (1 << 11);
+	const float x32 = x12 + x22;
+	const float b3  = (((ac1 * 4 + x32) * (1 << oss)) + 2) / 4;
+	const float x13 = ac3 * b6 / (1 << 13);
+	const float x23 = (b1 * (b6 * b6 / (1 << 12))) / (1 << 16);
+	const float x33 = ((x13 + x23) + 2) / 4;
+	const float b4  = ac4 * (uint32_t)(x33 + 32768) / (1 << 15);
+	const float b7  = ((uint32_t) raw_pressure - b3) * (50000 >> oss);
+	const float p   = (b7 < 0x80000000)
+										 ? (b7 * 2) / b4
+										 : (b7 / b4) * 2
+										 ;
+	const float x14 = (p / (1 << 8) * (p / (1 << 8))) * 3038 / (1 << 16);
+	const float x24 = (-7357 * p) / (1 << 16);
+
+	*true_temp = (b5 + 8) / 15 / 10;
+	*true_pressure = p + (x14 + x24 + 3791) / (1 << 4);
+	(void) cal;
+}
+
 
 void test_i2c(BMP_180* bmp)
 {
@@ -35,14 +84,20 @@ void test_i2c(BMP_180* bmp)
 
 int main(int argc, char** argv)
 {
-  BMP_180 bmp;
-  test_i2c(&bmp);
+  //BMP_180 bmp;
+  //test_i2c(&bmp);
 
-  for (int i = 0; i < 22; i ++)
-  {
-    printf("%.2X,", bmp.calibration_coefficients[i] & 0xff);
-  }
-  puts("");
+  //for (int i = 0; i < 22; i ++)
+  //{
+  //  printf("%.2X,", bmp.calibration_coefficients[i] & 0xff);
+  //}
+  //puts("");
+
+	float true_temp;
+	float true_press;
+
+	compute_true_temp_and_pressure(&true_temp, &true_press, 27898, 23843, 0, NULL);
+	printf("%f, %f\n", true_temp, true_press);
 
   return 0;
   (void) argc;
