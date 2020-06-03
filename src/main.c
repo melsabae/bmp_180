5eeaed2dd1de65889ae2c12bc2163aadd3ff0b00
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2020-06-02 22:54:07 -0500

    re-enable real logic

--- src/main.c
+++ src/main.c
@@ -6,38 +6,38 @@
 
 int main(int argc, char** argv)
 {
-	//int fd = 0;
-	//BMP_180_Calibration cal;
+	int fd = 0;
+	BMP_180_Calibration cal;
 
-	//setup_bmp_180(&fd, &cal);
+	setup_bmp_180(&fd, &cal);
 
-	//printf(
-	//		  "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n"
-	//		, cal.ac1
-	//		, cal.ac2
-	//		, cal.ac3
-	//		, cal.ac4
-	//		, cal.ac5
-	//		, cal.ac6
-	//		, cal.b1
-	//		, cal.b2
-	//		, cal.mb
-	//		, cal.mc
-	//		, cal.md
-	//		);
+	printf(
+			  "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n"
+			, cal.ac1
+			, cal.ac2
+			, cal.ac3
+			, cal.ac4
+			, cal.ac5
+			, cal.ac6
+			, cal.b1
+			, cal.b2
+			, cal.mb
+			, cal.mc
+			, cal.md
+			);
 
 	while(true)
 	{
-		debug_read_bmp_180();
+		//debug_read_bmp_180();
 
-		//for(size_t i = 0; i < 4; i ++)
-		//{
-		//	float temp     = 0.0f;
-		//	float press    = 0.0f;
-		//	float altitude = 0.0f;
-		//	read_bmp_180_all(&temp, &press, &altitude, fd, &cal, BMP_180_OSS_CONTROL_4);
-		//	printf("%lu, %f = degrees C, %f = pascals, %f = meters\n\n", i, temp, press, altitude);
-		//}
+		for(size_t i = 0; i < 4; i ++)
+		{
+			float temp     = 0.0f;
+			float press    = 0.0f;
+			float altitude = 0.0f;
+			read_bmp_180_all(&temp, &press, &altitude, fd, &cal, BMP_180_OSS_CONTROL_4);
+			printf("%lu, %f = degrees C, %f = pascals, %f = meters\n\n", i, temp, press, altitude);
+		}
 
 		usleep(1E6);
 	}
